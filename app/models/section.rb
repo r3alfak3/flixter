3a07955c4a5737b23949bc5f120add6676ac2b30
r3alfak3 <thenyartist@gmail.com>
2019-07-10 06:38:26 +0000

    Added more instructor functionality

---
+++ app/models/section.rb
@@ -0,0 +1,8 @@
+class Section < ApplicationRecord
+	belongs_to :course
+	has_many :sections
+
+	validates :title, presence: true
+	validates :description, presence: true
+	validates :cost, presence true. numericality: {greater_than_or_equal_to: 0}
+end

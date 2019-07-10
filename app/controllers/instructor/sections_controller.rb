ab37396bceb0b93a2eda19ab9358ada6c24f69ec
r3alfak3 <thenyartist@gmail.com>
2019-07-10 22:12:44 +0000

    Added Sections Submission

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,18 +1,18 @@
 class Instructor::SectionsController < ApplicationController
-	def new
-		@course = Course.find(params[:course_id])
-		@section = Section.new
-	end
+  def new
+    @course = Course.find(params[:course_id])
+    @section = Section.new
+  end
 
-	def create
-		@course = Course.find(params[:course_id])
-		@section = @course.sections.create(section_params)
-		redirect_to new_instructor_course_section(@course)
-	end
+  def create
+    @course = Course.find(params[:course_id])
+    @section = @course.sections.create(section_params)
+    redirect_to instructor_course_path(@course)
+  end
 
-	private
+  private
 
-	def section_params
-		params.require(:section).permit(:title)
-	end
+  def section_params
+    params.require(:section).permit(:title)
+  end
 end

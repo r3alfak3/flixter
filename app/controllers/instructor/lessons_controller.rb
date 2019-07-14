46dc5b3d2b459b55976b7dde29c060bab919346b
r3alfak3 <thenyartist@gmail.com>
2019-07-14 22:39:12 +0000

    Another test of errors

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -7,21 +7,21 @@ before_action :require_authorized_for_current_section
 	end
 
 	def create
-		@lesson = current_session.lessons.create(lesson_params)
+		@lesson = current_section.lessons.create(lesson_params)
 		redirect_to instructor_course_path(current_section.course)
 	end
 
 private
 
 	def require_authorized_for_current_section
-		if current_session.course.user != current_user
+		if current_section.course.user != current_user
 			render :plain 'Unauthorized', status: :unauthorized
 		end
 	end
 
 	helper_method :current_section
-	def current_session
-		@current_session = ||= Section.find(params[:section_id])
+	def current_section
+		@current_section = ||= Section.find(params[:section_id])
 	end
 
 	def lesson_params

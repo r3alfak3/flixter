c7d33b49ab21f8285d8072be5af04289e67d1931
r3alfak3 <stefanisaia@gmail.com>
2019-08-08 03:14:54 +0000

    Sections & Lessons are now sortable in the admin dashboard

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -11,20 +11,33 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'Success!'
+  end
+
   private
 
-    def require_authorized_for_current_course
-      if current_course.user != current_user
-        return render plain: 'Unauthorized', status: :unauthorized
-      end
+  def current_section
+    @section_lesson ||= Section.find(params[:id])
+  end
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
     end
+  end
 
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
   end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
